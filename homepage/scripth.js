c805b07d72fab8a05ce1482c7ffbde4f3e33b6c3
GOKULA KANNAN V <gokulakannanv04@gmail.com>
2024-02-17 19:20:46 +0530

    1702241920

--- homepage/scripth.js
+++
@@ -1,6 +0,0 @@
-const handlesubmit = function() {
-var na = document.getElementById('name');
-    var input =na.value;
-    // var getInput = input;
-    localStorage.setItem("storageName",input);
- }
